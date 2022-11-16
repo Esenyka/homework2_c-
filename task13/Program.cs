fdd4e44276123afeac50cb31e02c557d30e13e95
eJwtzE2PwiAUheH/wroLaCVt2fqRzGRiYsbJLAnQq0OLXIRWR43/XWK6OznJ8z6IMgZS2uMAngjSVhraqqYtVbXm2tS0YcArxjkwMIsDb0ooDWWkICHixXYQZ/qYS3KcU8c/lBu/6rE/19vzT7P6aod135f7z+Vmd/+4XwI1v9/M59SbyPEWIDsNKkLMbzL4PlR3sl6ESTtr5AC3IoLqBMZjHgGLKUEUcFLWFVeMw8HhNWP4DzZCkmokwk/OPZ8vJpVP6w== <esenia1317@gmail.com>
2022-11-16 13:28:24 +0700

    added task13

---
+++ task13/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите цифру : ");
+
+string number = Console.ReadLine();
+int l = number.Length;
+
+if (l < 3)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+if (Convert.ToInt32(number) < 0)
+{
+    Console.WriteLine(number[3]);
+}
+else
+{
+    Console.WriteLine(number[2]);
+}
