b4709f9e728795f4f493aed05e21a383238bf697
eJwtzE2PwiAUheH/wroLaCVt2fqRzGRiYsbJLAnQq0OLXIRWR43/XWK6OznJ8z6IMgZS2uMAngjSVhraqqYtVbXm2tS0YcArxjkwMIsDb0ooDWWkICHixXYQZ/qYS3KcU8c/lBu/6rE/19vzT7P6aod135f7z+Vmd/+4XwI1v9/M59SbyPEWIDsNKkLMbzL4PlR3sl6ESTtr5AC3IoLqBMZjHgGLKUEUcFLWFVeMw8HhNWP4DzZCkmokwk/OPZ8vJpVP6w== <esenia1317@gmail.com>
2022-11-16 13:36:22 +0700

    added task15, days of week

---
+++ task15/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите цифру, соответствующую дню недели(от 1 до 7) : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 5 && number < 8)
+{
+    Console.WriteLine("Да");
+}
+else if (number > 7)
+{
+    Console.WriteLine("От 1 до 7!!");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
